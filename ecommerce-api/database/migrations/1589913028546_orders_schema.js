1a7903286d9592f8e98fd059219105ff93368229
HervalMata <hervalmataamparo@gmail.com>
2020-05-19 15:42:50 -0300

    relationships done

--- ecommerce-api/database/migrations/1589913028546_orders_schema.js
+++ ecommerce-api/database/migrations/1589913028546_orders_schema.js
@@ -11,6 +11,14 @@ class OrdersSchema extends Schema {
       table.decimal('discount', 12, 2).notNullable().defaultTo(0.0)
       table.decimal('total', 12, 2).notNullable()
       table.enu('status', ['awaiting', 'cancelled', 'shipped', 'paid']).defaultTo('awaiting')
+      table.integer('cart_id').unsigned()
+
+      table
+        .foreign('cart_id')
+        .references('id')
+        .inTable('carts')
+        .onDelete('cascade')
+
       table.timestamps()
     })
   }
