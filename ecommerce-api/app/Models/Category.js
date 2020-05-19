01f19d018220adc5cca6725066325abe9e54c872
HervalMata <hervalmataamparo@gmail.com>
2020-05-19 15:02:29 -0300

    aula 12

--- ecommerce-api/app/Models/Category.js
+++ ecommerce-api/app/Models/Category.js
@@ -8,6 +8,10 @@ class Category extends Model {
   images() {
     return this.belongsTo('App/Models/Image')
   }
+
+  products() {
+    return this.belongsToMany('App/Models/Product')
+  }
 }
 
 module.exports = Category
